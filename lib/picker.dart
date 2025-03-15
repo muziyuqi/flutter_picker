16bb4bb7a1f844c543bdeef3f2db741d1ac245e2
muziyuqi <muziyuqi93@gmail.com>
2025-03-16 00:52:49 +0800

    fix bug
    打开 ../../../.pub-cache/hosted/pub.flutter-io.cn/flutter_picker-2.1.0/lib/picker.dart
    将第429行的 theme!.bottomAppBarColor 替换为 theme!.colorScheme.surface
    将第482行和第729行的 headline6 替换为 titleLarge

--- lib/picker.dart
+++ lib/picker.dart
@@ -426,7 +426,7 @@ class PickerWidgetState<T> extends State<_PickerWidget> {
                   bottom: BorderSide(color: theme!.dividerColor, width: 0.5),
                 ),
                 color: picker.headerColor == null
-                    ? (theme!.bottomAppBarColor)
+                    ? (theme!.colorScheme.surface)
                     : picker.headerColor,
               ),
         ));
@@ -479,7 +479,7 @@ class PickerWidgetState<T> extends State<_PickerWidget> {
       child: picker.title == null
           ? SizedBox()
           : DefaultTextStyle(
-              style: theme!.textTheme.headline6?.copyWith(
+              style: theme!.textTheme.titleLarge?.copyWith(
                     fontSize: Picker.DefaultTextSize,
                   ) ??
                   TextStyle(fontSize: Picker.DefaultTextSize),
@@ -726,7 +726,7 @@ abstract class PickerAdapter<T> {
                     fontFamily: picker?.state?.context != null
                         ? Theme.of(picker!.state!.context)
                             .textTheme
-                            .headline6!
+                            .titleLarge!
                             .fontFamily
                         : "",
                     fontSize: Picker.DefaultTextSize),
